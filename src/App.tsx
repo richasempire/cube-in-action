e24c6bdbb97a878a4269f40740665291cc3c4ec5
Richa Gupta <richagupta@C02DDER0MD6M.tld>
2024-05-19 16:18:53 -0700

    draggable - colour change is commented

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import Cube from './components/Cube';
 import './App.css'; 
+import { OrbitControls, Sky } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 
-const App: React.FC = () => {
+
+// const App: React.FC = () => {
+//   return (
+//     <div className="App">
+
+//       <canvas>
+//       <Sky/>
+
+//       </canvas>
+   
+     
+
+//     </div>
+//   );
+// };
+
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <Cube />
-    </div>
-  );
+      <Canvas>
+          <mesh>
+            <Sky/>
+    
+            <Cube/>
+          </mesh>
+          
+      </Canvas>
+  )
 };
 
-export default App;
+
+// export default App;
--- src/components/Cube.tsx
+++ src/components/Cube.tsx
@@ -1,27 +1,162 @@
-import React, { useState } from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useState, useRef, useEffect } from 'react';
+import { Canvas, useThree, useFrame } from '@react-three/fiber';
 import { OrbitControls, TransformControls, useCursor } from '@react-three/drei';
 import { useControls } from 'leva';
-import { create } from 'zustand';
+import { DragControls } from'../utils/dragControls';
+import { useDrag } from 'react-use-gesture';
+import create from 'zustand';
+import * as THREE from 'three';
+
+// Zustand store setup
+// const useStore = create((set) => ({
+//   target: null,
+//   setTarget: (target: any) => set({ target }),
+//   selectedVertex: null,
+//   setSelectedVertex: (vertex: any) => set({ selectedVertex: vertex }),
+// }));
 
 // Box component
-const Box = (props: JSX.IntrinsicElements['mesh']) => {
+// const Box = ({ vertices, setVertices }) => {
+//   const meshRef = useRef();
+//   const setTarget = useStore((state) => state.setTarget);
+//   const [hovered, setHovered] = useState(false);
+//   useCursor(hovered);
+
+//   useEffect(() => {
+//     const geometry = new THREE.BufferGeometry();
+//     const positions = new Float32Array(vertices.length * 3);
+//     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+//     meshRef.current.geometry = geometry;
+//     updateVertices();
+//   }, [vertices]);
+
+//   const updateVertices = () => {
+//     const positions = meshRef.current.geometry.attributes.position.array;
+//     vertices.forEach((vertex, i) => {
+//       positions[i * 3] = vertex.x;
+//       positions[i * 3 + 1] = vertex.y;
+//       positions[i * 3 + 2] = vertex.z;
+//     });
+//     meshRef.current.geometry.attributes.position.needsUpdate = true;
+//     meshRef.current.geometry.computeVertexNormals();
+//   };
+
+//   return (
+//     <mesh
+//       ref={meshRef}
+//       onClick={(e) => setTarget(e.object)}
+//       onPointerOver={() => setHovered(true)}
+//       onPointerOut={() => setHovered(false)}
+//     >
+//       <meshStandardMaterial color={hovered ? 'hotpink' : 'gray'} />
+//     </mesh>
+//   );
+// };
+
+// // EditableVertex component
+// const EditableVertex = ({ position, index }) => {
+//   const setSelectedVertex = useStore((state) => state.setSelectedVertex);
+//   const [hovered, setHovered] = useState(false);
+//   useCursor(hovered);
+
+//   return (
+//     <mesh
+//       position={position.toArray()}
+//       onClick={() => setSelectedVertex(index)}
+//       onPointerOver={() => setHovered(true)}
+//       onPointerOut={() => setHovered(false)}
+//     >
+//       <sphereGeometry args={[0.05, 16, 16]} />
+//       <meshStandardMaterial color={hovered ? 'yellow' : 'blue'} />
+//     </mesh>
+//   );
+// };
+
+// Main Cube component
+// export default function Cube() {
+//   const { target, setTarget, selectedVertex, setSelectedVertex } = useStore();
+//   const { mode } = useControls({ mode: { value: 'translate', options: ['translate', 'rotate', 'scale'] } });
+
+//   const [vertices, setVertices] = useState([
+//     new THREE.Vector3(-1, -1, -1),
+//     new THREE.Vector3(1, -1, -1),
+//     new THREE.Vector3(1, 1, -1),
+//     new THREE.Vector3(-1, 1, -1),
+//     new THREE.Vector3(-1, -1, 1),
+//     new THREE.Vector3(1, -1, 1),
+//     new THREE.Vector3(1, 1, 1),
+//     new THREE.Vector3(-1, 1, 1),
+//   ]);
+
+//   const updateVertexPosition = (index, position) => {
+//     const updatedVertices = vertices.map((vertex, i) => (i === index ? position.clone() : vertex));
+//     setVertices(updatedVertices);
+//   };
+
+//   return (
+//     <Canvas style={{ height: '100vh' }} onPointerMissed={() => setTarget(null)}>
+//       <ambientLight intensity={0.5} />
+//       <pointLight position={[10, 10, 10]} />
+//       <Box vertices={vertices} setVertices={setVertices} />
+//       {vertices.map((vertex, index) => (
+//         <EditableVertex key={index} position={vertex} index={index} />
+//       ))}
+//       {selectedVertex !== null && (
+//         <TransformControls
+//           object={new THREE.Object3D().copy(vertices[selectedVertex])}
+//           mode={mode}
+//           onObjectChange={(e) => updateVertexPosition(selectedVertex, e.target.position)}
+//         />
+//       )}
+//       <OrbitControls />
+//     </Canvas>
+//   );
+// }
+
+
+
+export default function Cube() {
+
+    const colors = ['hotpink', 'red', 'blue', 'green', 'yellow'];
+    const ref = useRef();
+    const [colorIdx, setColorIdx] = useState(0);
+    const [position, setPosition] = useState([0, 0, 0]);
+    const { size, viewport } = useThree();
+    const aspect = size.width / viewport.width;
+
+    useFrame(() => {
+     
+      });
+
+    const bind = useDrag(
+        ({ offset: [x, y] }) => {
+          const [, , z] = position;
+          setPosition([x / aspect, -y / aspect, z]);
+        },
+        { pointerEvents: true }
+      );
+
+
     return (
-      <mesh {...props}>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
-    );
-  };
-  
-  // Main Cube component
-  export default function Cube() {
-    return (
-      <Canvas style={{ height: '100vh' }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[0, 0, 0]} />
-        <OrbitControls />
-      </Canvas>
-    );
-  }
+        <mesh position={position}
+        {...bind()}
+        ref={ref}
+        onClick={(e) => {
+        //   if (colorIdx === 4) {
+        //     setColorIdx(0);
+        //   } else {
+        //     setColorIdx(colorIdx + 1);
+        //   }
+        }}
+        onPointerOver={(e) => console.log('hover')}
+        onPointerOut={(e) => console.log('unhover')}>
+            <boxGeometry attach={"geometry"}/>
+            <meshLambertMaterial attach="material" color={colors[colorIdx]} />
+            {/* <OrbitControls/> */}
+            <ambientLight intensity={0.9}/>
+            <pointLight />
+            {/* <meshStandardMaterial color = "grey" /> */}
+
+        </mesh>
+    )
+};
